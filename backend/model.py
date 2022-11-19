ac695a1e0b901231637aa95bb3a7412e51fe2683
Yuntong Miao <87073032+Yunry@users.noreply.github.com>
2022-11-19 16:32:02 +0100

    Update model.py
    optimized for stock

--- backend/model.py
+++ backend/model.py
@@ -3,7 +3,16 @@
 
 # Pydantic allows auto creation of JSON Schemas from models
 from pydantic import BaseModel
+from decimal import *
 
 class Todo(BaseModel):
     title: str
     description: str
+
+class Stock(BaseModel):
+    typ: str
+    name: str
+    side: str
+    qty: Decimal
+    price: Decimal
+    user: str
